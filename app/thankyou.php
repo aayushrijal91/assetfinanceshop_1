107935d8ff04589fea3657ee1b600c5eb45d69e2
Aayush Rijal <arijal@aiims.com.au>
2023-04-12 11:57:04 +1000

    Design 1 completed

---
+++ app/thankyou.php
@@ -0,0 +1,21 @@
+<?php include __DIR__ . '/functions.php'; ?>
+<?php $thankyou_page = true; ?>
+<?php include __DIR__ . '/src/header.php'; ?>
+
+<div class="thankyou-wrapper text-white text-center py-3" style="background-color: green;">
+    Thank you! Your message has been sent. We will contact you shortly.
+</div>
+
+<!-- Event snippet for Quote Form (AFS Main 1)** conversion page -->
+
+<script>
+    gtag('event', 'conversion', {
+        'send_to': 'AW-714867981/_2JoCIPcpo8YEI2K8NQC'
+    });
+</script>
+
+<?php include __DIR__ . '/src/nav.php'; ?>
+
+<?php include __DIR__ . '/src/main.php'; ?>
+
+<?php include __DIR__ . '/src/footer.php'; ?>
